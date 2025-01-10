ab2129b17cab2ddbf5d7b44ba4b2d90412ad7ba6
Tristan Poveda <povedatristan@gmail.com>
2025-01-10 10:40:44 +0100

    fix : NavMesh Updt

--- Assets/Game/Scripts/Enemies/NavAgentSystem.cs
+++ Assets/Game/Scripts/Enemies/NavAgentSystem.cs
@@ -3,15 +3,12 @@ using Unity.Entities;
 using Unity.Transforms;
 using UnityEngine;
 
-[BurstCompile]
+/*[BurstCompile]
 public partial class NavAgentSystem : ISystem
 {
     [BurstCompile]
     private void OnUpdate(ref SystemState state)
     {
-        /*foreach(var (navAgent, transform, entity) in SystemAPI.Query<RefRW<NavAgentComponent>, RefRW<LocalTransform>>().WithEntityAccess())
-        {
-
-        }*/
+        
     }
-}
+}*/
